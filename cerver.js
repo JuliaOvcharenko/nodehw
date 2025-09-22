8a4c416517b69da65ced5af07609490e2a2c9264
Julia Ovcharenko <samsung2100yuliyu@gmail.com>
2025-09-22 16:43:33 +0300

    Work with posts api

--- cerver.js
+++ cerver.js
@@ -1,18 +1,17 @@
 const express = require('express')
-const moment = require('moment')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
 const HOST = "localhost"
 const PORT = 8002
 
-let getTime = () => {
-    return moment().format('HH:mm:ss')
-}
+const postsPath = path.join(__dirname, "posts.json") 
+const posts = JSON.parse(fs.readFileSync(postsPath, 'utf-8'))
 
-
-app.get('/timestamp', (req, res) => {
-    res.json(getTime())
+app.get('/posts', (req, res) => {
+    res.status(200).json(posts)
 })
 
 app.listen(PORT, HOST, () => {
