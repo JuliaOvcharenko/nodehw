f4cc87bcaa2426ec163322941c8f0d63a41812ab
Julia Ovcharenko <samsung2100yuliyu@gmail.com>
2025-09-30 16:11:03 +0300

    work with query params

--- server.js
+++ server.js
@@ -20,7 +20,38 @@ app.get('/timestamp', (req, res) => {
 })
 
 app.get('/posts', (req, res) => {
-    res.status(200).json(posts)
+    take = Number(req.query.take)
+    skip = Number(req.query.skip)
+
+    if(!take && !skip){
+        res.status(200).json(posts)
+        return
+    }
+
+    if ((take && isNaN(+take)) || (skip && isNaN(+skip))){
+        res.status(400).json("take and skip must be a number")
+        return
+    }
+
+    if (take < 0 || skip < 0){
+        res.status(400).json("take and skip must be a positive number")
+        return
+    }
+    if (skip >= posts.length){
+        res.status(200).json([])
+        return
+    }
+    
+    if (!take){
+        res.status(200).json(posts.slice(skip))
+        return
+    }
+    if (!skip){
+        res.status(200).json(posts.slice(0, take))
+        return
+    }
+    res.status(200).json(posts.slice(skip, skip + take))
+
 })
 
 app.get('/posts/:id', (req, res) => {
@@ -41,6 +72,8 @@ app.get('/posts/:id', (req, res) => {
     }
     res.status(200).json(post)
 })
+
+
 
 app.listen(PORT, HOST, () => {
     console.log(`Cerver is running on http://localhost:8002`)
